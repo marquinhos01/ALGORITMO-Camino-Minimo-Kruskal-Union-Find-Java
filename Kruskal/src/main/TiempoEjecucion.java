eb41bd3fd4306a60a3f595b3aa0a0482c1cad2bc
@ro_natt <romysegretin@gmail.com>
2021-10-09 21:00:37 -0300

    Se agrega mediana a TiempoEjecución

--- Kruskal/src/main/TiempoEjecucion.java
+++ Kruskal/src/main/TiempoEjecucion.java
@@ -1,5 +1,7 @@
 package main;
 
+import java.util.Arrays;
+
 import algoritmos.KruskalBFS;
 import algoritmos.KruskalUF;
 import generador.Generador;
@@ -30,21 +32,47 @@ public class TiempoEjecucion {
 		tiempo = fin - inicio;
 		return tiempo;
 	}
+	
+	private static double calcularMediana(double[] datos){
+        double mediana;
+        Arrays.sort(datos);
+        if(datos.length % 2 == 0){
+            double sumaMedios = datos[datos.length/2] + datos[datos.length/2 - 1]; 
+            mediana = (double)sumaMedios / 2; 
+        } else {
+            mediana = datos[datos.length/2];
+        }
+        return mediana;
+    }
+	
 	public static void main(String[] args) {
-		Grafo g = Generador.grafoConexo(1000);
+		Grafo g = Generador.grafoConexo(50);
 		float promedioBFS=0;
 		float promedioUF=0;
+		double[] datosBFS = new double[10];
+		double[] datosUF = new double[10];
 		
 		for (int i=0 ; i<10 ; i++) {
 			TiempoEjecucion t = new TiempoEjecucion(g);
 			System.out.println("Ejecucion numero:" +(i+1));
-			System.out.println("Kruskal con BFS tardo : " + t.getTiempoEjecucionKruskalBFS());
-			System.out.println("Kruskal sin UF tardo : " + t.getTiempoEjecucionKruskal() + "\n");
-			promedioBFS+=t.getTiempoEjecucionKruskalBFS();
-			promedioUF+=t.getTiempoEjecucionKruskal();
+			
+			long tiempoBFS= t.getTiempoEjecucionKruskalBFS();
+			long tiempoUF= t.getTiempoEjecucionKruskal();
+			
+			System.out.println("Kruskal con BFS tardo : " + tiempoBFS);
+			System.out.println("Kruskal sin UF tardo : " + tiempoUF + "\n");
+			promedioBFS+=tiempoBFS;
+			promedioUF+=tiempoUF;
+			
+			datosBFS[i]= tiempoBFS;
+			datosUF[i]= tiempoUF;
 		}
 		System.out.println("El promedio del tiempo de BFS fue:" + promedioBFS/10.0);
 		System.out.println("El promedio del tiempo de UF fue:" + promedioUF/10.0);
+		
+		System.out.println("La tendencia central (usando la mediana) del tiempo de BFS fue:" + calcularMediana(datosBFS));
+		System.out.println("La tendencia central (usando la mediana) del tiempo de UF fue:" + calcularMediana(datosUF));
+		
 	}
 	
 }
